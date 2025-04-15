34e979def0a16adf7523cb6ab47be6ede8992ebb
Jannat <jannat.mim@student.tdsb.on.ca>
2025-04-15 03:42:43 +0000

    completedpracticeproblem

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -1,8 +1,31 @@
+import java.util.Scanner;
 public class PracticeProblem {
 
 	public static void main(String args[]) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Enter the number of rings: ");
+        int rings = scanner.nextInt();
 
-	}
+        int moves = towerOfHanoi(rings);
+
+        if (moves == -1) {
+        	 System.out.println("Invalid input. Must be 3 or more rings.");
+    	 } else {
+            System.out.println("Minimum number of moves: " + moves);
+        }
+    }
+
+	public static int towerOfHanoi(int num) {
+        if (num < 3) {
+            return -1;
+        }
+		
+        if (num == 3) {
+            return 7;
+        }
+
+        return 2 * towerOfHanoi(num - 1) + 1;
+    }
 
 	public static void q1() {
 		//Write question 1 code here
